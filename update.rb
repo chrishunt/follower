cd0a11bccda505177dc5b104335743e1fbdb5ea8
Chris Hunt <c@squareup.com>
2012-08-12 11:08:22 -0700

    Extract duplicate code in the janky ass script

--- update.rb
+++ update.rb
@@ -5,6 +5,25 @@ def colorize(text, code)
   "\e[#{code}m#{text}\e[0m"
 end
 
+def compare(comparison, previous, current)
+  Followers::TwitterClient.users_lookup(
+    Followers::Comparison.send(comparison, previous, current)
+  ).map { |f| f['screen_name'] }
+end
+
+def print_comparison(comparison, followers)
+  lost = comparison == :lost
+
+  message  = "\n#{followers.count} users have "
+  message += lost ? 'stopped' : 'started'
+  message += ' following you '
+  message += lost ? ':(' : ':)'
+  color    = lost ? 31 : 32
+
+  puts colorize(message, color)
+  puts followers.join(', ')
+end
+
 current_followers = Followers::TwitterClient.followers(TWITTER_USERNAME)
 puts "#{TWITTER_USERNAME} has #{current_followers.count} followers today"
 
@@ -13,23 +32,11 @@ previous_followers = Dir["#{DOWNLOAD_DIR}/*"].last
 if previous_followers
   previous_followers = JSON.parse(File.read(previous_followers))
 
-  lost = Followers::TwitterClient.users_lookup(
-    Followers::Comparison.lost(previous_followers, current_followers)
-  ).map { |f| f['screen_name'] }
+  lost   = compare(:lost, previous_followers, current_followers)
+  gained = compare(:gained, previous_followers, current_followers)
 
-  gained = Followers::TwitterClient.users_lookup(
-    Followers::Comparison.gained(previous_followers, current_followers)
-  ).map { |f| f['screen_name'] }
-
-  unless lost.empty?
-    puts colorize("\n#{lost.count} users have stopped following you :(", 31)
-    puts lost.join(', ')
-  end
-
-  unless gained.empty?
-    puts colorize("\n#{gained.count} users have started following you :)", 32)
-    puts gained.join(', ')
-  end
+  print_comparison(:lost, lost) unless lost.empty?
+  print_comparison(:gained, gained) unless gained.empty?
 end
 
 unless lost.empty? && gained.empty?
