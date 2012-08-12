85c74d747da31d338a787eeb049743af071d5050
Chris Hunt <c@squareup.com>
2012-08-12 10:24:29 -0700

    Add unit test for HttpClient

---
+++ spec/unit/http_client_spec.rb
@@ -0,0 +1,30 @@
+require 'http_client'
+
+describe HttpClient do
+  subject { HttpClient.new(url) }
+
+  let(:headers) {{ 'Content-Type' => 'application/json' }}
+  let(:url)     { 'http://example.com' }
+  let(:http)    { double('Net::HTTP').as_null_object }
+
+  before do
+    Net::HTTP.stub(:new => http)
+  end
+
+  describe '#get' do
+    it 'makes a get request without params' do
+      path = '/api'
+      http.should_receive(:get).with(path, headers)
+
+      subject.get(path)
+    end
+
+    it 'makes a get requests with params' do
+      path = '/api'
+      params = { :p1  => 1, :p2 => 'hello world' }
+      http.should_receive(:get).with("#{path}?p1=1&p2=hello%20world", headers)
+
+      subject.get(path, params)
+    end
+  end
+end
