2359c4c3562d4928add9a8545588ecaf929968b7
Chris Hunt <c@squareup.com>
2012-09-02 10:50:06 -0700

    Use ruby 1.9 hash syntax for config

--- config/followers.rb
+++ config/followers.rb
@@ -1,6 +1,6 @@
 CONFIG = OpenStruct.new(
-  :url       => 'https://twitter.com',
-  :api       => 'https://api.twitter.com/1',
-  :username  => 'chrishunt',
-  :downloads => 'downloads'
+  url:         'https://twitter.com',
+  api:         'https://api.twitter.com/1',
+  username:    'chrishunt',
+  downloads:   'downloads'
 ).freeze
