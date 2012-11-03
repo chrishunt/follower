3b8fe6b244447f5e059b6ea00fe91005c8a74561
Chris Hunt <c@squareup.com>
2012-11-03 14:03:07 -0600

    Use ruby 1.9 hash syntax

--- lib/followers/twitter_client.rb
+++ lib/followers/twitter_client.rb
@@ -3,9 +3,7 @@ module Followers
     extend self
 
     def followers(username)
-      result = http.get('/followers/ids.json', {
-        :screen_name => username
-      })
+      result = http.get('/followers/ids.json', { screen_name: username })
       JSON.parse(result.body)['ids']
     end
 
@@ -13,9 +11,7 @@ module Followers
       users = []
       # must request users in batches of 100
       until (batch = ids.slice!(0..99)).empty?
-        result = http.get('/users/lookup.json', {
-          :user_id => batch.join(',')
-        })
+        result = http.get('/users/lookup.json', { user_id: batch.join(',') })
         users << JSON.parse(result.body)
       end
       users.flatten
