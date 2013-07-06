36a7073b6b989a843325e47ec2b0bb7f0c911904
Chris Hunt <chrishunt@github.com>
2013-07-06 09:24:20 -0700

    Prefix twitter settings with twitter_

--- config/followers.rb
+++ config/followers.rb
@@ -1,7 +1,7 @@
 CONFIG = OpenStruct.new(
-  username: ENV['TWITTER_USERNAME'],
-  consumer_key: ENV['TWITTER_CONSUMER_KEY'],
-  consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
+  twitter_username: ENV['TWITTER_USERNAME'],
+  twitter_consumer_key: ENV['TWITTER_CONSUMER_KEY'],
+  twitter_consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
 
   deliver_email: ENV['FOLLOWERS_DELIVERY_EMAIL'],
   gmail_username: ENV['FOLLOWERS_GMAIL_USERNAME'],
